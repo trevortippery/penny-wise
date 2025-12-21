15152e10c507aea613da8a44f3185a10513aff0a
trevortippery <trevortippery221@gmail.com>
2025-12-20 20:55:56 -0800

    Update category.test.js

--- __tests__/category.test.js
+++ __tests__/category.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("../app/app");
 const pool = require("../db/db");
+const testUtils = require("./tests-util");
 
 afterEach(async () => {
   await pool.query("DELETE FROM transactions");
@@ -13,9 +14,12 @@ afterAll(async () => {
 });
 const testEmail = "categorytest@example.com";
 const testPassword = "password123";
+const categoriesRoute = "/api/categories";
 
 describe("POST /api/categories", () => {
   let userId;
+  let authToken;
+
   // Create test user
   beforeEach(async () => {
     const userResponse = await request(app).post("/api/auth/register").send({
@@ -24,15 +28,24 @@ describe("POST /api/categories", () => {
     });
 
     userId = userResponse.body.user.id;
+
+    const loginResponse = await request(app).post("/api/auth/login").send({
+      email: testEmail,
+      password: testPassword,
+    });
+
+    authToken = loginResponse.body.token;
   });
 
-  //first test a valid category creation
+  // first test a valid category creation
   test("should create a category from a test user", async () => {
-    const response = await request(app).post("/api/categories").send({
-      userId: userId,
-      name: "Utilies",
-      color: "FF5733",
-    });
+    const response = await request(app)
+      .post(categoriesRoute)
+      .set("Authorization", `Bearer ${authToken}`)
+      .send({
+        name: "Utilities",
+        color: "FF5733",
+      });
 
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty(
@@ -41,47 +54,62 @@ describe("POST /api/categories", () => {
     );
     expect(response.body.category).toHaveProperty("id");
     expect(response.body.category).toHaveProperty("user_id", userId);
-    expect(response.body.category).toHaveProperty("name", "Utilies");
+    expect(response.body.category).toHaveProperty("name", "Utilities");
     expect(response.body.category).toHaveProperty("color", "FF5733");
   });
 
-  // Test required fields (userId, name, color) are not present
-  test("should 400 error when userId is not present", async () => {
-    await testPost400Error(
-      {
-        name: "Utilies",
+  // Tests for token is present and invalid token
+  test("should 401 error when no token provided", async () => {
+    await testUtils.testError({
+      route: categoriesRoute,
+      testUnit: {
+        name: "Utilities",
         color: "FF5733",
       },
-      "User id is required",
-    );
+      statusCode: 401,
+      message: "No token provided",
+      crudAction: "POST",
+    });
   });
 
-  test("should 400 error when name is not present", async () => {
-    await testPost400Error(
-      {
-        userId: userId,
+  test("should 401 error with invalid token", async () => {
+    await testUtils.testError({
+      route: categoriesRoute,
+      testUnit: {
+        name: "Utilities",
         color: "FF5733",
       },
-      "Name is required",
-    );
+      authToken: "invalid-token",
+      statusCode: 401,
+      message: "Invalid or expired token",
+      crudAction: "POST",
+    });
+  });
+
+  // Tests for required fields are present
+  test("should 400 error when name is not present", async () => {
+    await testUtils.testError({
+      route: categoriesRoute,
+      testUnit: {
+        color: "FF5733",
+      },
+      authToken: authToken,
+      statusCode: 400,
+      message: "Name is required",
+      crudAction: "POST",
+    });
   });
 
   test("should 400 error when color is not present", async () => {
-    await testPost400Error(
-      {
-        userId: userId,
-        name: "Utilies",
+    await testUtils.testError({
+      route: categoriesRoute,
+      testUnit: {
+        name: "Utilities",
       },
-      "Color is required",
-    );
+      authToken: authToken,
+      statusCode: 400,
+      message: "Color is required",
+      crudAction: "POST",
+    });
   });
-
-  //Test field types are correct
 });
-
-async function testPost400Error(testUnit, message) {
-  const response = await request(app).post("/api/categories").send(testUnit);
-
-  expect(response.status).toBe(400);
-  expect(response.body).toHaveProperty("error", message);
-}
