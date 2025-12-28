37f66d8a58b42c55307842f0424bbd5edec6d02f
trevortippery <trevortippery221@gmail.com>
2025-12-28 15:48:40 -0800

    work in progress: add basic transaction table html structure and grid layout for the dashboard page

--- app/client/js/dashboard.js
+++ app/client/js/dashboard.js
@@ -1,14 +1,6 @@
-// // Check if token exists
-// const token = localStorage.getItem("token");
+const token = localStorage.getItem("token");
 
-// if (!token) {
-//   // No token found, redirect to login
-//   window.location.href = "/";
-// }
-
-const body = document.querySelector("body");
-const newDiv = document.createElement("div");
-const newH1 = document.createElement("h1");
-newH1.textContent = "Made it to the dashboard!";
-newDiv.appendChild(newH1);
-body.appendChild(newDiv);
+if (!token) {
+  // No token found, redirect to login
+  window.location.href = "/";
+}
