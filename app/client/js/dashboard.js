af2df8d260210059c09bab620930deb6dc2ccda9
trevortippery <trevortippery221@gmail.com>
2025-12-29 22:54:32 -0800

    feat: add transaction and category forms

--- app/client/js/dashboard.js
+++ app/client/js/dashboard.js
@@ -4,3 +4,9 @@ if (!token) {
   // No token found, redirect to login
   window.location.href = "/";
 }
+
+const signOutButton = document.querySelector(".sign-out");
+signOutButton.addEventListener("click", () => {
+  localStorage.clear();
+  window.location.href = "/";
+});
---
+++ app/client/js/transactions.js
@@ -0,0 +1,31 @@
+async function loadCategories() {
+  const token = localStorage.getItem("token");
+
+  try {
+    const response = await fetch("/api/categories/", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const categories = await response.json();
+
+    const categorySelect = document.getElementById("category");
+
+    categorySelect.innerHTML =
+      "<option value='' disabled selected>Select a category</option";
+
+    categories.categories.forEach((category) => {
+      const option = document.createElement("option");
+      option.value = category.id;
+      option.textContent = category.name;
+      categorySelect.appendChild(option);
+    });
+  } catch (error) {
+    console.error("Error loading categories:", error);
+    alert("Failed to load categories");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadCategories());
